f9e67824e9c903e6e91a2f1620444b38dccb52ee
Amogh Mulge <amoghrm111@gmail.com>
2019-12-04 00:39:22 +0530

    Update application.py

--- flask/atm/application.py
+++ flask/atm/application.py
@@ -4,15 +4,11 @@ import re
 
 app = Flask(__name__)
 
-# Secret key for sessions
 app.secret_key = "secret"
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-	# Initialises balance and transaction count from session variables
-	# If session keys do not exist, KeyError raised - (occurs the first time program is run)
-	# Session variables initialised
-	# To clear session - session.clear()
+	
 	try:
 		balance = session["balance"]
 		count = session["count"]
@@ -26,36 +22,33 @@ def index():
 
 	if request.method == "POST":
 
-		# Checks if amount field is empty
 		if request.form["amount"] == "" :
 			msg = "Amount is required"
 			return render_template("index.html", balance=balance, msg=msg)
 
-		# Checks if amount entered is negative
+		
 		if int(request.form["amount"]) < 0 :
 			msg = "Cannot enter negative amount"
 			return render_template("index.html", balance=balance, msg=msg)
 
-		# Checks if number of transactions is equal to 5
+		
 		if count == 5:
 			msg = "5 transactions complete"
 			return render_template("index.html", balance=balance, msg=msg)
 
-		# Checks if user clicked on Withdraw
+		
 		if request.form["action"] == 'Withdraw':
 
-			# Checks if amount is greater than balance
+			
 			if int(request.form["amount"]) > session["balance"] :
 				msg = "Cannot withdraw amount greater than balance"
 				return render_template("index.html", balance=balance, msg=msg)
 
-			# Checks if amount is greater than 5000
+			
 			elif int(request.form["amount"]) > 5000 :
 				msg = "Cannot withdraw amount greater than 5000"
 				return render_template("index.html", balance=balance, msg=msg)
 
-			# Deducts amount entered from balance and stores in session
-			# Updates the number of transactions by one
 			else:
 				balance = balance - int(request.form["amount"])
 				session["balance"] = balance
@@ -63,16 +56,13 @@ def index():
 				msg = "Money Withdrawn"
 				return render_template("index.html", balance=balance, msg=msg)
 
-		# Checks if user clicked on Deposit
 		elif request.form["action"] == 'Deposit':
 
-			# Checks if amount is greater than 10000
+			
 			if int(request.form["amount"]) > 10000 :
 				msg = "Cannot deposit amount greater than 10000"
 				return render_template("index.html", balance=balance, msg=msg)
 
-			# Adds amount entered to balance and stores in session
-			# Updates the number of transactions by one
 			else:
 				balance = balance + int(request.form["amount"])
 				session["balance"] = balance
