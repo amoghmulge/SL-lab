ca52a22e79569a95a8dc5c9c1e102cb4e2b08ad0
Amogh Mulge <amoghrm111@gmail.com>
2019-12-04 08:56:28 +0530

    Update ShoppingCart.py

--- flask/shoppingcart/ShoppingCart.py
+++ flask/shoppingcart/ShoppingCart.py
@@ -2,7 +2,6 @@ from flask import Flask, redirect, render_template, request, session, url_for
 
 app = Flask(__name__)
 
-# Secret key for sessions
 app.secret_key = "secret"
 
 @app.route("/", methods=["GET", "POST"])
@@ -10,10 +9,6 @@ def store():
     if request.method == "GET":
         return render_template("store.html")
 
-    # For each item in store, checks if item is in session
-    # If item is in session, increments the count by value entered in the form
-    # If item is not in session, initialises the count by value entered in the form
-    # Redirects to the HTML page to view Shopping Cart
     if request.method == "POST":
         for item in ["eggs", "milk", "bread"]:
             if item not in session :
@@ -26,8 +21,7 @@ def store():
 @app.route("/cart", methods=["GET", "POST"])
 def cart():
 
-    # Creates a list of dictionaries containing each item in cart and its quantity
-    # Displays this list in a HTML page
+    
     cart = []
     for item in ["eggs", "milk", "bread"]:
         cart.append({"name":item.capitalize(), "quantity":session[item]})
@@ -37,18 +31,14 @@ def cart():
 @app.route("/buy", methods=["GET", "POST"])
 def buy():
 
-    # Total amount initialised to 0
     amount = 0
 
-    # Index for list containing prices of items
+
     index = 0
 
-    # Prices of every item
+
     prices = [5, 12, 22]
 
-    # Creates a list of dictionaries containing each item in cart, its quantity, and cost(price*qty)
-    # Calculates total bill amount
-    # Displays the bill in a HTML page
     cart = []
     for item in ["eggs", "milk", "bread"]:
         row = {}
